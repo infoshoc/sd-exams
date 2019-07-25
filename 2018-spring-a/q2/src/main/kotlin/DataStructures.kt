ede689b2ca1a7d507decb76752359cef7b63e88d
infoshoc <infoshoc@infoshoc.infoshoc>
2019-07-25 13:12:46 +0300

    2018 spring a q2

---
+++ 2018-spring-a/q2/src/main/kotlin/DataStructures.kt
@@ -0,0 +1,8 @@
+object DataStructures {
+    fun <T> register(t: T)
+            where T: TookMatam,
+                  T: TookCombi
+    {
+        //
+    }
+}
---
+++ 2018-spring-a/q2/src/main/kotlin/Main.kt
@@ -0,0 +1,3 @@
+fun main(){
+    DataStructures.register(Itay)
+}
